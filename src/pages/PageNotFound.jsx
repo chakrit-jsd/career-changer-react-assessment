f3450fdd7214daafbf86caa8ba657a231f94d3fa
chakrit-jsd <chakritabv@gmail.com>
2023-04-19 11:10:38 +0700

    add navbar on page404

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,13 @@
+import Navbar from "../components/Navbar"
+
 const PageNotFound = () => {
     return (
+      <>
+        <Navbar />
         <div id='pagenotfound' className="row">
             <h1>PAGE NOT FOUND</h1>
         </div>
+      </>
     )
 }
 
